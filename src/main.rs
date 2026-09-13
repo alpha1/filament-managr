32f0216e3d7f6260ef70b8352363c683a782984c
Michael <admin@alpha1beta.com>
2026-09-13 03:34:37 -0400

    Started refactor to allow for multiple libraries, started work related to saving the library to a json file in app data (or changeable at a later point), and autosave of the library as actions are being taken.

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,149 @@
 #![allow(unused)]
 #![allow(unused_imports)]
-use std::{error::Error, io, io::stdin, process,env,collections::BTreeMap};//use serde_ };
+use core::error;
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::{
+	error::Error,
+	io,
+	io::stdin,
+	io::Write,
+	fs,
+	fs::File,
+	fs::OpenOptions,
+	process,
+	env,
+	collections::BTreeMap,
+};//use serde_ };
 use app_data::AppData;
+use uuid::Uuid;
 //use std::ops::Index;
 use csv::WriterBuilder;
 use dialoguer::Select;
 use dialoguer::Input;
-//use console::Term;
-//use json::json;
+use serde::{Deserialize,Serialize};
 //use csv::WriterBuilder;
 //use std::fmt::Debug;
 //use std::io::*;
 //use std::alloc::System;
 //use std::process::Command;
-#[derive(Clone, Debug)]
+//use console::Term;
 
-struct FilamentInventory {
-	all_filament:Vec<Filament>
-	//all_filament:HashMap<i32,Filament>
+#[derive(Clone, Debug)]
+struct FilamentLibrary {
+	pub library_name:String,
+	pub id: Uuid,
+	pub all_filament:Vec<Filament>,
+	recycle_bin:Vec<Filament>,
 }
 
-impl FilamentInventory {
-	fn new() -> Self {
+#[derive(Debug)]
+struct FilamentLibrarySourceFile {
+	source_file:File,
+}
+
+#[derive(Clone, Debug,serde::Serialize, serde::Deserialize)]
+struct Filament {
+	name: String,
+	manufacturer: String,
+	material: String,
+	color: String,
+	//spool_count: i8,
+}
+#[derive(Clone, Debug )]
+struct Spool {
+	name: Filament,
+	weight: i8,
+	material: String,
+	color: String,
+	//spool_count: i8,
+}
+
+impl Filament {
+	/*fn new() -> Self {
 		Self {
+			name:String::new(),
+			filament_type:String::new(),
+			color:String::new(),
+			//spool_count::new()
+		}
+	}*/
+}
+
+impl FilamentLibrary {
+	fn new( name: String) -> Self {
+		Self {
+			library_name:name,
+			id: Uuid::new_v4(),
 			all_filament:Vec::new(),
-			//all_filament:HashMap::new(),
+			recycle_bin:Vec::new(),
 		}
 	}
-	/*
-	fn getInventory( &self ){
-		println!("Getting Inventory");
-		//self.all_filament;
+
+	fn create_library_file( &self ){
+		let mut app_save_directory = get_app_save_directory();
+		let mut app_autosave_directory = get_app_autosave_directory();
+
+		std::fs::create_dir_all(app_save_directory.clone() );
+		std::fs::create_dir_all(app_autosave_directory.clone() );
+		app_save_directory.push(self.id.clone().to_string() );
+		
+
+		let mut sourcefile_full_path = app_save_directory;
+		sourcefile_full_path.set_extension("json");
+		println!("{}",sourcefile_full_path.display());
+
+		let sourcefile = OpenOptions::new()
+			.read(true)
+			.write(true)
+			.create_new(true)
+			.open(sourcefile_full_path);
+
+		match sourcefile {
+			Ok(mut file) => {
+				writeln!(&mut file, "new line");
+			},
+			Err(error) => {
+				println!("Crap!");
+				println!("{}",error);
+			}
+		}
+		
+   		
 	}
-	*/
+
+	fn open_library_file(){
+
+		//let metadata = file.metadata()?;
+	}
 
 	fn export_csv( &self ) -> Result<(), Box<dyn std::error::Error>> {
 		println!("Starting Export");
-		
-		let app_data = AppData::new("FilamentManagr");
-		let data_dir = app_data.ensure_data_dir().unwrap();
-
-		println!("data_dir: {}", data_dir.display());
 
 		  let mut wtr = WriterBuilder::new().from_path("Filament Inventory.csv")?;
-			wtr.write_record(&["Filament Name","Filament Menufacturer", "Filament Material", "Filament Color"])?;
-			for (_pos, item ) in self.all_filament.iter().enumerate() {
-				wtr.write_record(&[item.name.clone(), item.manufacturer.clone(), item.material.clone(), item.color.clone()])?;
+			wtr.write_record(["Filament Name","Filament Menufacturer", "Filament Material", "Filament Color"])?;
+			for ( item ) in self.all_filament.iter() {
+				wtr.write_record([item.name.clone(), item.manufacturer.clone(), item.material.clone(), item.color.clone()])?;
 			}
 			wtr.flush()?;
 			Ok(())
 	}
 
-	/*
+	
 	fn export_json( &self ){
 
 	}
-	*/
+
+	fn autosave_filament_library_json( &self ) -> Result<(), Box<dyn std::error::Error>> {
+		let autosave_inventory = self.get_inventory_copy();
+
+		//let inventory_library = vec![]
+
+		let autosave_json = serde_json::to_string( &autosave_inventory )?;
+		println!("{}", autosave_json);
+		Ok(())
+	}
+	
 
 	fn import_csv( &self ) -> Result<(),Box<dyn Error>> {
 		println!("Starting Import");
@@ -78,9 +166,14 @@ impl FilamentInventory {
 	}
 	
 	fn list_inventory( &self ){
+		println!( "Listing filaments in {}", self.library_name);
 		for (pos, item ) in self.all_filament.iter().enumerate() {
 			println!("[{}]: {} - {} - {} - {}", pos, item.name, item.manufacturer, item.material, item.color );
 		}
+	}
+
+	fn get_inventory_copy( &self ) -> Vec<Filament>{
+		self.all_filament.clone()
 	}
 	
 	fn maybe_add_filament( &mut self ){
@@ -94,7 +187,7 @@ impl FilamentInventory {
 		let mut spool_count_string = String::new();
 
 		let mut filament_name_loop_completed = false;
-		while filament_name_loop_completed == false {
+		while !filament_name_loop_completed {
 			println!("What will this filament be called?");
 			match stdin().read_line(&mut filament_name) {
 				Ok(_n) => 
@@ -109,7 +202,7 @@ impl FilamentInventory {
 		}
 
 		let mut filament_manufacturer_loop_completed = false;
-		while filament_manufacturer_loop_completed == false {
+		while !filament_manufacturer_loop_completed {
 			println!("Who manufacturers this filament?");
 			match stdin().read_line(&mut filament_manufacturer) {
 				Ok(_n) => 
@@ -125,7 +218,7 @@ impl FilamentInventory {
 		}
 
 		let mut filament_material_loop_completed = false;
-		while filament_material_loop_completed == false {
+		while !filament_material_loop_completed {
 			filament_material = String::new();
 			println!("What material is this filament? Select a number of the materials, or type a filament type.");
 			list_filament_types();
@@ -184,7 +277,7 @@ impl FilamentInventory {
 		}
 
 		let mut filament_color_loop_completed = false;
-		while filament_color_loop_completed == false {
+		while !filament_color_loop_completed {
 			
 		println!("What color is this filament?");
 			match stdin().read_line(&mut filament_color) {
@@ -200,7 +293,7 @@ impl FilamentInventory {
 		}
 
 		let mut filament_spool_count_loop_completed = false;
-		while filament_spool_count_loop_completed == false {
+		while !filament_spool_count_loop_completed {
 			
 			println!("How many spools would you like to ad?");
 			match stdin().read_line(&mut spool_count_string) {
@@ -228,28 +321,29 @@ impl FilamentInventory {
 			color: String::from(filament_color.trim()),
 			//spool_count: spool_count,
 		};*/
-		let mut _new_filament = self.add_filament(filament_name.clone(), filament_material.clone(),filament_manufacturer.clone(), filament_color.clone() );
+		self.add_filament(filament_name.clone(), filament_material.clone(),filament_manufacturer.clone(), filament_color.clone() );
+
+
 		//filaventory.push(new_filament);
+		//println!("Added {} to inventory.", new_filament.);
 		
 		println!( "You NOW have {} items in your current inventory.", self.all_filament.len() );
 	}
-	
+
 	fn add_filament( &mut self, filament_name:String, filament_menufacturer:String, filament_material:String, filament_color:String  ){
 		println!("Adding Filament to Inventory");
 
 		if in_filament_materials(filament_material.clone() ){
-
+			let new_filament = Filament {
+				name: String::from(filament_name.trim()),
+				manufacturer: filament_menufacturer.trim().to_string(),
+				material: String::from(filament_material.trim()),
+				color: String::from(filament_color.trim()),
+				//spool_count: spool_count,
+			};
+			self.all_filament.push(new_filament);
 		}
-
-		let new_filament = Filament {
-			name: String::from(filament_name.trim()),
-			manufacturer: String::from(filament_menufacturer).trim().to_string(),
-			material: String::from(filament_material.trim()),
-			color: String::from(filament_color.trim()),
-			//spool_count: spool_count,
-		};
-		self.all_filament.push(new_filament);
-
+		self.autosave_filament_library_json();
 	}
 
 	fn is_filament_allowed( &self ){
@@ -303,29 +397,12 @@ impl FilamentInventory {
 	
 }
 
-#[derive(Clone, Debug)]
-struct Filament {
-	name: String,
-	manufacturer: String,
-	material: String,
-	color: String,
-	//spool_count: i8,
-}
 
-impl Filament {
-	/*fn new() -> Self {
-		Self {
-			name:String::new(),
-			filament_type:String::new(),
-			color:String::new(),
-			//spool_count::new()
-		}
-	}*/
-}
+
 
 fn main() {
 	println!("##########DEBUG INFO##########");
-	
+
 	let args: Vec<String> = env::args().collect();
     dbg!(args.clone() );
 
@@ -337,76 +414,124 @@ fn main() {
 	println!("##############################");
 
 	println!( "Welcome to Filament Managr.");
+	let mut main_menu = BTreeMap::new();
+	main_menu.insert("o", "Open Existing Filament Library");
+	main_menu.insert("c", "Create Filament Library");
+	main_menu.insert("q", "Quit");
 
-	let mut inventory = FilamentInventory::new();
-	
-	println!( "Inventory has {} items.", inventory.all_filament.len() );
-
-	let mut commands = BTreeMap::new();
-	commands.insert("1", "List inventory");
-	commands.insert("2", "Add filament to inventory");
-	commands.insert("3", "Delete filament to inventory");
-	commands.insert("9", "List filament materials");
-	commands.insert("t", "Test filament material names");
-	commands.insert("e", "Export filament inventory to CSV");
-	commands.insert("x", "Setup example filaments");
-	commands.insert("h", "Help");
-	commands.insert("q", "Quit");
-	//inventory.push(
-
-	let mut commands_vec = vec!["List Inventory", "Add Filament"];
-
+	let mut library_name = String::new();
 	loop {
-		let mut input = String::new();
-		println!("What would you like to do? Enter the character or character inside brackets to select an option.");
-		for (key, value) in &commands {
+		println!("Get started.");
+		let mut main_menu_selection = String::new();
+		for (key, value) in &main_menu {
 			println!("[{key}]: {value}");
 			
 		}
-		 match stdin().read_line(&mut input) {
-			Ok(_n) =>  {
-				/*
-				println!("You entered: {}", input);
-				dbg!( &input );
-				dbg!( &input.trim() );
-				*/
-				match input.as_str().trim() {
-					//"1" => listFilament( &inventory ),
-					"1" => inventory.list_inventory(),
-					//"2" => addFilamentToCollection( &mut inventory ),
-					"2" => inventory.maybe_add_filament(),
-					//"3" => maybeRemoveFilament( &mut inventory ),
-					"3" => inventory.maybe_remove_filament(),
-					"9" => list_filament_types( ), 
-					"0" => {
-						println!("Goodbye");
-						process::exit(0);
+
+		match stdin().read_line(&mut main_menu_selection) {
+				Ok(_n) =>  {
+					/*
+					println!("You entered: {}", input);
+					dbg!( &input );
+					dbg!( &input.trim() );
+					*/
+					match main_menu_selection.as_str().trim() {
+						"o" => {
+							println!("Select a Filament Library to Open");
+							get_app_data_directory();
+							get_app_default_save_directory_files();
+							
+						},
+						"c" => {
+							println!("What should we call this library?");
+							
+							match stdin().read_line(&mut library_name) {
+								Ok(_n) => {
+									library_name = library_name.trim().to_string();
+									
+									
+								}
+								Err(_) => todo!()
+							}
+
+						},
+						"q"  => {
+
+						},
+						&_ => println!("Error"),
 					}
-					"x" => inventory.import_test_filaments(),
-					"e" => inventory.export_csv().expect("FAIL"),
-					//"j" => inventory.export_json(),
-					"i" => inventory.import_csv().expect("test"),
-					"t" => inventory.is_filament_allowed(),
-					&_ => println!("Error"),
-				}
+
+				},
+				Err(_) => todo!()
+		}
+		
+		let mut inventory = FilamentLibrary::new( library_name.clone() );
+		inventory.create_library_file();
+	
+		
+		println!( "Library {} has {} items.", inventory.library_name, inventory.all_filament.len() );
+		
+		let mut commands = BTreeMap::new();
+		commands.insert("1", "List inventory");
+		commands.insert("2", "Add filament to inventory");
+		commands.insert("3", "Delete filament to inventory");
+		commands.insert("9", "List filament materials");
+		commands.insert("t", "Test filament material names");
+		commands.insert("e", "Export filament inventory to CSV");
+		commands.insert("x", "Setup example filaments");
+		commands.insert("h", "Help");
+		commands.insert("q", "Quit");
+		commands.insert("m", "Return to main menu");
+		//inventory.push(
+		loop {
+			let mut input = String::new();
+			println!("What would you like to do? Enter the character or character inside brackets to select an option.");
+			for (key, value) in &commands {
+				println!("[{key}]: {value}");
+				
 			}
-			Err(error) => println!( "Invalid input. Error: {error}." )
+			match stdin().read_line(&mut input) {
+				Ok(_n) =>  {
+					/*
+					println!("You entered: {}", input);
+					dbg!( &input );
+					dbg!( &input.trim() );
+					*/
+					match input.as_str().trim() {
+						//"1" => listFilament( &inventory ),
+						"1" => inventory.list_inventory(),
+						//"2" => addFilamentToCollection( &mut inventory ),
+						"2" => inventory.maybe_add_filament(),
+						//"3" => maybeRemoveFilament( &mut inventory ),
+						"3" => inventory.maybe_remove_filament(),
+						"9" => list_filament_types( ), 
+						"0" => {
+							println!("Goodbye");
+							process::exit(0);
+						}
+						"x" => inventory.import_test_filaments(),
+						"e" => inventory.export_csv().expect("FAIL"),
+						//"j" => inventory.export_json(),
+						"i" => inventory.import_csv().expect("test"),
+						"t" => inventory.is_filament_allowed(),
+						"m" => break(),
+						&_ => println!("Error"),
+					}
+				}
+				Err(error) => println!( "Invalid input. Error: {error}." )
+			}
 		}
 	}
 }
 
 fn get_filament_materials() -> [&'static str; 11] {
-	return ["PLA","PLA-HT","PETG","ASA","PCTG","NYLON","TPE","TPU","PET","PVA","PP"];
+	["PLA","PLA-HT","PETG","ASA","PCTG","NYLON","TPE","TPU","PET","PVA","PP"]
 }
 
 fn in_filament_materials( filament_material:String)  -> bool {
 	println!("{filament_material}");
 	let filament_materials = get_filament_materials();
-	if filament_materials.iter().any(|&i| i== filament_material){
-		return true;
-	} else {
-		return false;
-	}
+	filament_materials.iter().any(|&i| i== filament_material)
 }
 
 fn list_filament_types( ) {
@@ -420,8 +545,59 @@ fn list_filament_types( ) {
 	
 }
 
+fn get_app_data_directory() -> PathBuf {
+	let app_data = AppData::new("FilamentManagr");
+	let data_dir = app_data.ensure_data_dir().unwrap();
+	data_dir
+}
+
+fn get_app_autosave_directory() -> PathBuf{
+	let mut data_dir = get_app_data_directory();
+	data_dir.push("autosave" );
+	data_dir
+}
+
+fn get_app_save_directory() -> PathBuf {
+	get_app_default_save_directory()
+}
+
+fn get_app_default_save_directory() -> PathBuf{
+	let mut data_dir = get_app_data_directory();
+	data_dir.push("libraries" );
+	data_dir
+}
+
+fn get_app_default_save_directory_files() -> Result<(), Box<dyn std::error::Error>>{
+	let mut data_dir = get_app_data_directory();
+	data_dir.push("libraries" );
+	if data_dir.is_dir() {
+		for entry in fs::read_dir(data_dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            println!("{}", entry.path().display());
+        }
+	}
+	Ok(())
+}
+
+
+
+#[cfg(test)]
+mod tests {
+	 #[test]
+    fn test_add_single_filament() {
+		use crate::FilamentLibrary;
+		let mut inventory = FilamentLibrary::new();
+		inventory.add_filament( "Overture PLA Pro".to_string(), "Overture".to_string(), "PLA".to_string(), "Black".to_string() );
+		let all_filament = inventory.get_inventory_copy();
+		
+		assert_eq!(all_filament.len(),1);
+    }
+
+}
+
 /*
-fn listFilament( inventory:&FilamentInventory ) {
+fn listFilament( inventory:&FilamentLibrary ) {
 	println!( "Listing Filament" );
 	println!( "Inventory has {} items.", inventory.all_filament.len() );
 	
@@ -433,7 +609,7 @@ fn listFilament( inventory:&FilamentInventory ) {
 */
 
 /*
-fn addFilamentToCollection( inventory:&mut FilamentInventory ){
+fn addFilamentToCollection( inventory:&mut FilamentLibrary ){
 	println!("Let's add some filament.");
 	println!( "You have {} items in your current inventory.", inventory.all_filament.len() );
 	println!("What will this filament be called?");
@@ -478,11 +654,11 @@ fn addFilamentToCollection( inventory:&mut FilamentInventory ){
 }
 */
 /*
-fn maybeRemoveFilament( inventory:&mut FilamentInventory ){
+fn maybeRemoveFilament( inventory:&mut FilamentLibrary ){
 	
 }
 
-fn removeFilamentFromCollection( inventory:&mut FilamentInventory ){
+fn removeFilamentFromCollection( inventory:&mut FilamentLibrary ){
 	let _removed_filament = inventory;
 	//inventory.remove(index_pos);
 	//println!( "Removed {}", removed_filament.name ); 
